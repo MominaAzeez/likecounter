83487c92119ded0e75ccfe0f374e72651cacb78e
MominaAzeez <mominaaziz234@gmail.com>
2025-11-03 09:52:14 -0800

    Create downcounter.py

---
+++ downcounter.py
@@ -0,0 +1,8 @@
+class DownCounter(UpDownCounter):
+    def __init__(self, start=0, step=1):
+        super().__init__(start)
+        self.step = step
+
+    def advance(self):
+        self.current -= self.step
+        return self.current
